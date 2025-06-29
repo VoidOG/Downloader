4244ca2397b97828b1b7ed3279b9498bbe51aec0
VoidOG <VoidGod123@outlook.com>
2025-06-30 03:14:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -220,7 +220,7 @@ def broadcast(update: Update, context: CallbackContext) -> None:
     )
 
 def main() -> None:
-    updater = Updater("7488772903:AAFJLlUJ2WYxxDRjha8eOsXn6miCPoqRL48", use_context=True)
+    updater = Updater("7488772903:AAGMlaLXvGXMfIW7Riv5FX7ehGT0_MJ-Vac", use_context=True)
 
     dispatcher = updater.dispatcher
 
